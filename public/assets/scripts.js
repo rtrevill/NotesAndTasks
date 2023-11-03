a853a5cf748598a9d10c9ce0a9d613b158ce9bac
rtrevill <139879271+rtrevill@users.noreply.github.com>
2023-11-03 13:28:17 +1100

    got POST and write to file working

--- public/assets/scripts.js
+++ public/assets/scripts.js
@@ -3,8 +3,6 @@ const notesRouter = require('../../routes/notes')
 
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send("Hello there");
-});
+app.use('/api', notesRouter);
 
 module.exports = app;
--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const notes = require('express').Router();
-const db = require('../db/db.json')
+const db = require('../db/db.json');
+const {readFile, writeFile} = require('fs').promises;
 
 notes.use(express.json());
 notes.use(express.urlencoded({ extended: true }));
@@ -11,7 +12,32 @@ notes.get('/notes', (req,res) => {
 
 notes.post('/notes', (req, res) => {
     console.log(req.body.title, req.body.text);
-    res.send('Received');
+    const{title, text} = req.body;
+
+    if ((!title)||(!text)){
+        res.send('please enter valid details')
+    };
+
+    // const newish  = JSON.parse(db)
+    // .then(console.log('file read successfully'));
+
+    const newTask = {
+        title,
+        text
+    }
+
+    db.push(newTask);
+    writeFile('./db/db.json', JSON.stringify(db));
+    res.send(db);
+
+    // async function respond(newish, newTask){
+    //     console.log(newish);
+    // await newish.push(newTask);
+    // res.send(newish);
+    // }
+
+    // respond(newish, newTask);
+    // res.send(listItems);
 })
 
 module.exports = notes;
