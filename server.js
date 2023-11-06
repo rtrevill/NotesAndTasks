b79c1236a9b3092631b6e8ed1010c26c295a4d5e
rtrevill <139879271+rtrevill@users.noreply.github.com>
2023-11-06 15:51:44 +1100

    created README.md began cleaning up code

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const app = express();
 const api = require('./public/assets/scripts');
 const path = require('path');
 
-
 const PORT = process.env.PORT||3001
 
+
 app.use(express.static('public'));
+
 app.use('/api', api);
 
 app.get('/notes', (req, res) => {
@@ -16,7 +17,6 @@ app.get('/notes', (req, res) => {
 app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
-
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
